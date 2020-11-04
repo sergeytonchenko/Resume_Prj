3edc8a59a4590f1f654789b2514386f632e1c8ce
sergeytonchenko <randick56@gmail.com>
2020-11-04 22:44:44 +0200

    dist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'jquery-validation';
 import 'boxicons/css/boxicons.min.css';
 import '@sass/main.scss';
-import {valid} from './js/validate-form'
-
+import {valid} from './js/validate-form';
+import './js/hamburger'
 
 $(document).ready(function(){
-    valid;
+    valid; 
 })
 
 
@@ -55,15 +55,18 @@ reveal.onload = function () {
     sr.reveal('.about__img', {})
     sr.reveal('.about__subtitle', {delay: 200})
     sr.reveal('.about__text', {delay: 400})
+    sr.reveal('.about__education', {delay: 600})
     
     sr.reveal('.skills__subtitle', {})
     sr.reveal('.skills__text', {delay: 200})
     sr.reveal('.skills__data', {interval: 200})
-    sr.reveal('.skills__img', {delay: 400})
+    sr.reveal('.skills__img', {delay: 400})    
     
     sr.reveal('.work__img', {interval: 200})
     
-    sr.reveal('.contact__input', {interval: 200})    
+    sr.reveal('.contact__input', {interval: 200})
+    sr.reveal('.contact-data', {delay: 200}) 
+    sr.reveal('.contact__location', {delay: 400})     
 }
 
 
