5b233b4ba78e3eb901763824170922afeaff4fb9
sergeytonchenko <randick56@gmail.com>
2020-11-05 16:55:37 +0200

    animate fix

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import 'jquery-validation';
 import 'boxicons/css/boxicons.min.css';
 import '@sass/main.scss';
 import {valid} from './js/validate-form';
+import {animateSkills} from './js/animate_skills';
 import './js/hamburger';
 import './js/reveal';
 import './js/nav-toggle';
 
 $(document).ready(function(){
     valid; 
+    animateSkills;
     $('.home__arrow').on('click', function(){        
         let dest = document.querySelector('.about'); 
           $('html,body').animate({ 
